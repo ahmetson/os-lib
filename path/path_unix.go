292d6847330542028522a1bbc6fbce980c00b410
Medet Ahmetson <admin@blocklords.io>
2023-08-14 17:56:13 +0300

    add all os related packages from service-lib

--- path/path_unix.go
+++ path/path_unix.go
@@ -1,12 +1,13 @@
 //go:build !windows
 // +build !windows
 
-package dev
+package path
 
 import (
 	"path/filepath"
 )
 
-func (c *Context) BinPath(url string) string {
+// BinPath Creates a binary file
+func BinPath(url string, name string) string {
 	return filepath.Join(c.Bin, url+"/bin")
 }
