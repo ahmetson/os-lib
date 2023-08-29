e1473d3f3f9696bf5d0e91e2e22584fda3473627
Medet Ahmetson <ahmetson@zoho.com>
2023-08-29 07:15:44 +0400

    fix the env test

--- env/file_test.go
+++ env/file_test.go
@@ -1,7 +1,9 @@
 package env
 
 import (
+	"github.com/ahmetson/os-lib/path"
 	"os"
+	"path/filepath"
 	"testing"
 
 	"github.com/stretchr/testify/suite"
@@ -17,25 +19,31 @@ type TestEnvSuite struct {
 
 // Make sure that Account is set to five
 // before each test
-func (suite *TestEnvSuite) SetupTest() {
-	suite.envPath = ".test.env"
-	os.Args = append(os.Args, suite.envPath)
+func (test *TestEnvSuite) SetupTest() {
+	currentDir, err := path.CurrentDir()
+	test.Require().NoError(err)
 
-	file, err := os.Create(suite.envPath)
-	suite.Require().NoError(err)
+	test.envPath = filepath.Join(currentDir, ".test.env")
+	os.Args = append(os.Args, test.envPath)
+
+	file, err := os.Create(test.envPath)
+	test.Require().NoError(err)
 	_, err = file.WriteString("")
-	suite.Require().NoError(err, "failed to write the data into: "+suite.envPath)
+	test.Require().NoError(err, "failed to write the data into: "+test.envPath)
 	err = file.Close()
-	suite.Require().NoError(err, "delete the dump file: "+suite.envPath)
+	test.Require().NoError(err, "delete the dump file: "+test.envPath)
+}
+
+func (test *TestEnvSuite) TearDownTest() {
+	err := os.Remove(test.envPath)
+	test.Require().NoError(err, "delete the dump file: "+test.envPath)
 }
 
 // All methods that begin with "Test" are run as tests within a
 // suite.
-func (suite *TestEnvSuite) TestRun() {
+func (test *TestEnvSuite) TestRun() {
 	err := LoadAnyEnv()
-	suite.Require().NoError(err)
-	err = os.Remove(suite.envPath)
-	suite.Require().NoError(err, "delete the dump file: "+suite.envPath)
+	test.Require().NoError(err)
 }
 
 // In order for 'go test' to run this suite, we need to create
