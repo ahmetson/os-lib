dc4a00adb4365dc59c7b99580847b720ab5b9f46
Medet Ahmetson <ahmetson@zoho.com>
2023-08-29 07:19:14 +0400

    fix leaning out the files after testing the path package

--- path/path_test.go
+++ path/path_test.go
@@ -14,86 +14,92 @@ import (
 // returns the current testing orchestra
 type TestPathSuite struct {
 	suite.Suite
-	envPath  string // using env as a file to check
-	fileName string
-	dir      string
+	envPath    string // using env as a file to check
+	currentDir string
+	fileName   string
+	dir        string
 }
 
 // Make sure that Account is set to five
 // before each test
-func (suite *TestPathSuite) SetupTest() {
-	suite.fileName = ".test.env"
-	suite.dir = "configs"
-	suite.envPath = filepath.Join("configs", ".test.env")
+func (test *TestPathSuite) SetupTest() {
+	test.fileName = ".test.env"
+	test.dir = "configs"
+	test.envPath = filepath.Join("configs", ".test.env")
 
 	currentDir, err := CurrentDir()
-	suite.Require().NoError(err)
+	test.Require().NoError(err)
+	test.currentDir = currentDir
 
-	err = MakeDir(filepath.Join(currentDir, suite.dir))
-	suite.Require().NoError(err)
+	err = MakeDir(filepath.Join(currentDir, test.dir))
+	test.Require().NoError(err)
 
-	file, err := os.OpenFile(filepath.Join(currentDir, suite.envPath), os.O_CREATE|os.O_RDWR, 0755)
-	suite.Require().NoError(err)
+	file, err := os.OpenFile(filepath.Join(currentDir, test.envPath), os.O_CREATE|os.O_RDWR, 0755)
+	test.Require().NoError(err)
 
 	_, err = file.WriteString("KEY=123")
-	suite.Require().NoError(err, "failed to write the data into: "+suite.envPath)
+	test.Require().NoError(err, "failed to write the data into: "+test.envPath)
 	err = file.Close()
-	suite.Require().NoError(err, "delete the dump file: "+suite.envPath)
+	test.Require().NoError(err, "close the file: "+test.envPath)
+}
+
+func (test *TestPathSuite) TearDownTest() {
+	absPath := filepath.Join(test.currentDir, test.envPath)
+	err := os.Remove(absPath)
+	test.Require().NoError(err, "delete the dump file: "+absPath)
+
 }
 
 // All methods that begin with "Test" are run as tests within a
 // suite.
-func (suite *TestPathSuite) TestRun() {
+func (test *TestPathSuite) TestRun() {
 	currentDir, err := CurrentDir()
-	suite.Require().NoError(err)
-	suite.Require().True(filepath.IsAbs(currentDir))
+	test.Require().NoError(err)
+	test.Require().True(filepath.IsAbs(currentDir))
 
 	// AbsDir
-	expected := filepath.Join(currentDir, suite.envPath)
-	absPath := AbsDir(currentDir, suite.envPath)
-	suite.Require().Equal(expected, absPath)
+	expected := filepath.Join(currentDir, test.envPath)
+	absPath := AbsDir(currentDir, test.envPath)
+	test.Require().Equal(expected, absPath)
 
 	// FileName
-	suite.Require().Equal(suite.fileName, FileName(absPath))
+	test.Require().Equal(test.fileName, FileName(absPath))
 
 	// DirAndFileName
 	actualDir, actualFileName := DirAndFileName(absPath)
-	suite.Require().Equal(filepath.Join(currentDir, suite.dir), actualDir)
-	suite.Require().Equal(suite.fileName, actualFileName)
+	test.Require().Equal(filepath.Join(currentDir, test.dir), actualDir)
+	test.Require().Equal(test.fileName, actualFileName)
 
 	// FileExist
 	fmt.Printf("abs path: %s\n", absPath)
 	exist, err := FileExist(absPath)
-	suite.Require().NoError(err)
-	suite.Require().True(exist)
+	test.Require().NoError(err)
+	test.Require().True(exist)
 
 	_, err = FileExist(actualDir)
-	suite.Require().Error(err) // checking directory should throw an error
+	test.Require().Error(err) // checking directory should throw an error
 
 	// DirExist
 	exist, err = DirExist(actualDir)
-	suite.Require().NoError(err)
-	suite.Require().True(exist)
+	test.Require().NoError(err)
+	test.Require().True(exist)
 
 	_, err = DirExist(absPath) // checking against file name should throw error
-	suite.Require().Error(err)
+	test.Require().Error(err)
 
 	// MakeDir
 	newPath := AbsDir(currentDir, "lvl_1/lvl_2/lvl_3")
 	err = MakeDir(newPath)
-	suite.Require().NoError(err)
+	test.Require().NoError(err)
 
 	err = MakeDir(newPath) // creating the existing directory skips it.
-	suite.Require().NoError(err)
+	test.Require().NoError(err)
 
 	err = MakeDir(absPath) // it includes the file names
-	suite.Require().Error(err)
-
-	err = os.Remove(suite.envPath)
-	suite.Require().NoError(err, "delete the dump file: "+suite.envPath)
+	test.Require().Error(err)
 
 	err = os.RemoveAll(newPath)
-	suite.Require().NoError(err, "deleting dump files failed")
+	test.Require().NoError(err, "deleting dump files failed")
 }
 
 // In order for 'go test' to run this suite, we need to create
